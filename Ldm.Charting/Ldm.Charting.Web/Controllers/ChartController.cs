4bc2094f7f419eb9fc617e93439e79a2934d83ff
ewatts <ewatts@leap.tfs.local>
2014-06-13 02:21:07 +0000

    Rolling back toms changes
    git-tfs-id: [http://syd-schbuild-06:8080/tfs/LdmRepository]$/LeapSearching/Internal Tools/InfoCharting/Branches/Development;C9142

--- Ldm.Charting/Ldm.Charting.Web/Controllers/ChartController.cs
+++ Ldm.Charting/Ldm.Charting.Web/Controllers/ChartController.cs
@@ -8,8 +8,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.DataVisualization.Charting;
-using System.Configuration;
-using System.Diagnostics;
 
 
 namespace Ldm.Charting.Web.Controllers
@@ -18,9 +16,6 @@ namespace Ldm.Charting.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.RabbitMqServerUrl = ConfigurationManager.AppSettings["rabbit_mq_server_url"];
-            ViewBag.MsmqMonitoring = ConfigurationManager.AppSettings["perf_counters_machine_name"] != null;
-               
             return View();
         }
 
@@ -42,7 +37,6 @@ namespace Ldm.Charting.Web.Controllers
             chart.SaveImage(ms);
             return File(ms.GetBuffer(), @"image/png");
         }
-
         public FileResult CreateQueueCountsChart(int width = 1000, int height = 1000)
         {
             IQueueCounterRepository repo = new QueueCounterRepository();
@@ -70,42 +64,6 @@ namespace Ldm.Charting.Web.Controllers
             MemoryStream ms = new MemoryStream();
             chart.SaveImage(ms);
             return File(ms.GetBuffer(), @"image/png");
-        }
-
-        public FileResult CreateMsmqCountsChart(int width = 1000, int height = 1000)
-        {
-            IQueueCounterRepository repo = new QueueCounterRepository();
-
-            Chart chart = ChartBuilder.BuildChart(SeriesChartType.Bar, width, height);
-            
-            var currentSeries = chart.Series[0];
-            AddMsmqCounter(currentSeries, "Report Orders", "reporting_data_synchronization");
-            AddMsmqCounter(currentSeries, "Errored Messages", "error");
-
-            // Set 
-            var maximum = currentSeries.Points.Select(x => x.YValues.First()).Max();
-            chart.ChartAreas[0].AxisY.Maximum = Math.Max(maximum + 10, 150);
-
-            // Save to MemoryStream and pass contents back to View.
-            MemoryStream ms = new MemoryStream();
-            chart.SaveImage(ms);
-            return File(ms.GetBuffer(), @"image/png");
-        }
-
-        private static void AddMsmqCounter(Series currentSeries, string name, string queueName)
-        {
-            try
-            {
-                var reportOrders = new PerformanceCounter("MSMQ Queue", "Messages in Queue",
-                    ConfigurationManager.AppSettings["perf_counters_machine_name"] + "\\private$\\" + queueName,
-                    ConfigurationManager.AppSettings["perf_counters_machine_name"]);
-
-                currentSeries.Points.AddXY(name, (int)reportOrders.RawValue);
-            }
-            catch
-            {
-                currentSeries.Points.AddXY(name + " (N/A)", 0);
-            }
         }
 
         // Test JSON output for Google Charts.
