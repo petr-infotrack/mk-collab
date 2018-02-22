9c6a382730fb62588a6ccc6ffe3f03b9e47f2242
Amber Weightman - InfoTrack <amber.weightman@infotrack.com.au>
2018-02-22 13:35:42 +1100

    GLOB-485 Add Serilog to Ldm.Charting Wallboard

---
+++ Ldm.Charting/Ldm.Charting.Data/ElasticClientSingleton.cs
@@ -0,0 +1,33 @@
+﻿using Nest;
+using System;
+
+namespace Ldm.Charting.Data
+{
+    public class ElasticClientSingleton
+    {
+        private static ElasticClient instance;
+
+        private ElasticClientSingleton() { }
+
+        public static ElasticClient Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = GetElasticClient();
+                }
+                return instance;
+            }
+        }
+
+        private static ElasticClient GetElasticClient()
+        {
+            var url = new Uri(System.Configuration.ConfigurationManager.AppSettings["elasticLogsEndpoint"]);
+            var settings = new ConnectionSettings(url)
+                .DisableDirectStreaming();
+            var client = new ElasticClient(settings);
+            return client;
+        }
+    }
+}
---
+++ Ldm.Charting/Ldm.Charting.Data/ErrorOccurrences.cs
@@ -0,0 +1,53 @@
+﻿using Nest;
+using System;
+
+namespace Ldm.Charting.Data
+{
+    [ElasticsearchType(Name = "logevent")]
+    public class ErrorOcccurences
+    {
+        [Text(Name = "messageTemplate")]
+        public string ErrorMessage { get; set; }
+
+        [Text(Name = "error_counts")]
+        public int Occurrences { get; set; }
+
+        public int FirstOccurrence { get; set; }
+
+        [Text(Name = "@timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [Text(Name = "level")]
+        public string Level { get; set; }
+
+        [Text(Name = "fields")]
+        public Fields Fields { get; set; }
+
+        public ErrorOcccurences() { }
+
+        public ErrorOcccurences(string errorMessage, int occurrences, int firstOccurrence)
+        {
+            ErrorMessage = errorMessage;
+            Occurrences = occurrences;
+            FirstOccurrence = firstOccurrence;
+        }
+    }
+
+    public class Fields
+    {
+        public string MachineName { get; set; }
+
+        public string Application { get; set; }
+
+        [Text(Name = "orderid")]
+        public string OrderIdStr { get; set; }
+
+        [Text(Name = "OrderId")]
+        public int OrderIdInt { get; set; }
+
+        public string Environment { get; set; }
+
+        public string Region { get; set; }
+    }
+
+}
